1ab897a990ed0fa357aacbe9281e65aaef561d79
Rocky Craig <rocky.craig@hp.com>
2015-07-19 10:49:04 -0600

    Much more Pythonic, especially w.r.t. FRY

--- repl_client.py
+++ repl_client.py
@@ -5,135 +5,114 @@ import socket_handling
 import json
 import json_handler
 
-def repl_init():
-    socket_handling.client_init()
-    sys
+from pdb import set_trace
+from pprint import pprint
 
-def command_quit(var):
-    print("bye")
-    return None
-
-def command_help(var):
-    print("help - list supported commands")
-    print("version - query Librarian for current version")
-    print("book_list <rowid> - list book details by book rowid")
-    print("book_listall - list book details for all books in database")
-    print("shelf_list - list shelf details by shelf name")
-    print("shelf_listall - list shelf details for all shelves in database")
-    print("shelf_create <shelf_name> <shelf_owner> - create new shelf")
-    print("shelf_resize <shelf_name> <size_in_bytes> - resize shelf to given size in bytes, add/remove books")
-    print("shelf_destroy <shelf_name> - destroy shelf and free reserved books")
-    print("shelf_open <shelf_name> - open shelf and setup node access")
-    print("shelf_close <shelf_name> - close shelf and tear down node access")
-    return ""
-
-def command_version(command):
-    command_dict = {}
-    command_dict["command"] = "version"
-    return(command_dict)
-
-def command_book_list(command):
-    command_dict['command'] = "book_list"
-    command_dict['rowid'] = command[1]
-    return(command_dict)
-
-def command_shelf_list(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_list"
-    command_dict['shelf_name'] = command[1]
-    return(command_dict)
-
-def command_shelf_listall(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_reservation_list"
-    return(command_dict)
-
-def command_shelf_create(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_create"
-    command_dict['shelf_name'] = command[1]
-    command_dict['shelf_owner'] = command[2]
-    return(command_dict)
-
-def command_shelf_resize(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_resize"
-    command_dict['shelf_name'] = command[1]
-    command_dict['size_bytes'] = command[2]
-    return(command_dict)
-
-def command_shelf_destroy(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_destroy"
-    command_dict['shelf_name'] = command[1]
-    return(command_dict)
-
-def command_shelf_open(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_open"
-    command_dict['shelf_name'] = command[1]
-    command_dict['res_owner'] = command[2]
-    return(command_dict)
-
-def command_shelf_close(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_close"
-    command_dict['shelf_name'] = command[1]
-    command_dict['res_owner'] = command[2]
-    return(command_dict)
-
-def command_shelf_reservation_list(command):
-    command_dict = {}
-    command_dict['command'] = "shelf_reservation_list"
+def keyvals2dict(command, values, keywords=None):
+    """keywords is an ordered list that aligns with values"""
+    assert isinstance(values, list), 'Didn\'t see that coming'
+    command_dict = { 'command': command }
+    if keywords is not None:
+        assert len(values) == len(keywords), 'Keywords<->values mismatch'
+        command_dict.update(dict(zip(keywords, values)))
     return command_dict
+
+def command_version(command, values):
+    """- query Librarian for current version"""
+    return keyvals2dict(command, values)
+
+def command_book_list(command, values):
+    """<rowid> - list book details by book rowid"""
+    return keyvals2dict(command, values, keywords=('rowid', ) )
+
+def command_shelf_list(command, *args):
+    """- list shelf details by shelf name"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', ) )
+
+def command_shelf_listall(command, *args):
+    """- list shelf details for all shelves in database"""
+    return keyvals2dict('shelf_reservation_list', *args)
+
+def command_shelf_create(command, *args):
+    """shelf_create <shelf_name> <shelf_owner> - create new shelf"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', 'shelf_owner') )
+
+def command_shelf_resize(command, *args):
+    """<shelf_name> <size_in_bytes> - resize shelf to given size in bytes, add/remove books"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', 'size_bytes') )
+
+def command_shelf_destroy(command, *args):
+    """<shelf_name> - destroy shelf and free reserved books"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', ) )
+
+def command_shelf_open(command, *args):
+    """<shelf_name>  <res_owner> - open shelf and setup node access"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', 'res_owner') )
+
+def command_shelf_close(command, *args):
+    """<shelf_name> <res_owner> - close shelf and tear down node access"""
+    return keyvals2dict(command, *args, keywords=('shelf_name', 'res_owner') )
+
+def command_shelf_reservation_list(command, *args):
+    """- show all shelves"""
+    return keyvals2dict(command, *args)
 
 # This is to stop the send currently this will be improved at a later date
 def json_error_creator(val):
     print("Invlaid Command")
     return ""
 
-def unknown_command_handler():
-    return lambda x : json_error_creator(x)
-
-
-command_handlers = defaultdict(unknown_command_handler)
-
-command_handlers.update({
-        "version":command_version,
-        "shelf_create": command_shelf_create,
-        "shelf_open": command_shelf_open,
-        "shelf_close": command_shelf_close,
-        "shelf_destroy": command_shelf_destroy,
-        "shelf_resize": command_shelf_resize,
-        "book_list": command_book_list,
-        "shelf_list": command_shelf_list,
-        "shelf_reservation_list": command_shelf_reservation_list,
-        "help" : command_help,
-        "quit" : quit
-        })
-
-command_line_helpers = defaultdict(unknown_command_handler)
-
 def main():
 
-    client = socket_handling.Client()
-    client.connect()
+    command_handlers = dict(
+        [ (name[8:], func) for (name, func) in globals().items() if
+            name.startswith('command_')
+        ]
+    )
+    docstrings = [ name + ' ' + func.__doc__ for (name, func) in
+        list(command_handlers.items()) ]
+    assert None not in docstrings, 'Missing one or more command_ docstrings'
+    docstrings = '\n'.join(sorted(docstrings))
 
-    processor = json_handler.Processor()
-    processor.add_processor(json.dumps)
+    # client = socket_handling.Client()
+    # client.connect()
+
+    # processor = json_handler.Processor()
+    # processor.add_processor(json.dumps)
 
     while True:
         user_input_list = input("command> ").split(' ')
-        command = user_input_list[0]
-
-        msg = command_handlers[command](user_input_list)
-        if msg is None:
-            break
-        if msg is "":
+        try:
+            assert user_input_list, 'Missing command'
+            command = user_input_list.pop(0)
+            if not command:
+                continue
+            if command == 'help':
+                print(docstrings)
+                continue
+            if command in ('exit', 'quit', 'q'):
+                break
+            handler = command_handlers[command]
+        except KeyError as e:
+            print('Unknown command "%s"' % command)
             continue
 
-        out_string = processor.process(msg)
-        print(client.send_recv(out_string))
+        try:
+            # user_input_list may be an empty list but is never None
+            cmdict = ''
+            cmdict = handler(command, user_input_list)
+        except AssertionError as e:
+            print(e)
+        except Exception as e:
+            print('Internal error:', str(e))
+
+        if not cmdict:
+            continue
+
+        pprint(cmdict)
+
+        # out_string = processor.process(cmdict)
+        # print(client.send_recv(out_string))
 
 if __name__ == '__main__':
     main()
