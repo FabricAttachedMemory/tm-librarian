eea9393939a2fde57a099068c9e63b83aa5c742d
Greg Pearson <greg.pearson@hp.com>
2015-09-08 17:28:50 -0600

    fixed handling of verbose argument, made program output more friendly for a script to use in a regression test and fixed pep8 warnings

--- repl_client.py
+++ repl_client.py
@@ -10,12 +10,14 @@ import socket_handling
 
 import cmdproto
 
-verbose = True
+verbose = 1
 
 #--------------------------------------------------------------------------
 
+
 def setup_command_generator(uil_repeat=None, script=None):
-    if script is None: # never raise StopIteration.  Caller watches loop counter
+    # Never raise StopIteration, caller watches loop counter
+    if script is None:
         while True:
             if uil_repeat:
                 yield copy.copy(uil_repeat)
@@ -26,11 +28,12 @@ def setup_command_generator(uil_repeat=None, script=None):
     with open(script, 'r') as f:
         script = f.readlines()
     for line in script:
-        line = line.strip() # gets EOL newline, too
+        line = line.strip()  # gets EOL newline, too
         if line and line[0] != '#':
-             yield line.split()
+            yield line.split()
 
 #--------------------------------------------------------------------------
+
 
 def main(node_id, serverhost, onetime=None):
     """Optionally connect to the server, then wait for user input to
@@ -47,10 +50,11 @@ def main(node_id, serverhost, onetime=None):
     }
     lcp = cmdproto.LibrarianCommandProtocol(context)
 
-    client = socket_handling.Client(selectable=False)
+    client = socket_handling.Client(selectable=False, verbose=verbose)
     try:
         client.connect(host=serverhost)
-        print('Connected')
+        if verbose > 1:
+            print('Connected')
     except Exception as e:
         print(str(e), '; falling back to local print')
         client = None
@@ -92,11 +96,11 @@ def main(node_id, serverhost, onetime=None):
                 print('\nAux commands:')
                 print('loop count delay_secs real_command ...')
                 print('script filename')
-                print('verbose on|off')
+                print('verbose <level> (cur = %d)' % verbose)
                 print('q or quit')
                 continue
             if command == 'verbose':
-                verbose = user_input_list[0].lower() == 'on'
+                verbose = int(user_input_list[1])
                 continue
             if command == 'loop':
                 loop = int(user_input_list[1])
@@ -116,17 +120,19 @@ def main(node_id, serverhost, onetime=None):
 
             # Construct and execute the command
             cmdict = lcp(command, *user_input_list[1:])
-            if verbose:
-                print()
+            if verbose == 2:
+                print(cmdict)
+            elif verbose > 2:
                 pprint(cmdict)
             if client:
                 client.send_all(cmdict)
                 rspdict = client.recv_all()
                 if 'errmsg' in rspdict:
                     pprint(rspdict)
-                if verbose:
+                if verbose == 1 or verbose == 2:
+                    print(rspdict['value'])
+                elif verbose > 2:
                     pprint(rspdict['value'])
-            print()
             time.sleep(delay)
 
             if not script:
@@ -159,25 +165,34 @@ def main(node_id, serverhost, onetime=None):
 
 
 if __name__ == '__main__':
-    import argparse, os, sys
 
-    parser = argparse.ArgumentParser(description='Librarian command-line client')
-    parser.add_argument('--verbose',
-                    help='level of runtime output, larger == more',
-                    type=int,
-                    default=0)
-    parser.add_argument('node_id',
-                    help='Numeric node id',
-                    type=int)
-    parser.add_argument('hostname',
-                    help='ToRMS host running the Librarian',
-                    type=str)
-    parser.add_argument('onetime',
-                    help='ONe-time command (then exit)',
-                    nargs='*',
-                    default=None)
+    import argparse
+    import os
+    import sys
+
+    parser = argparse.ArgumentParser(
+        description='Librarian command-line client')
+    parser.add_argument(
+        '--verbose',
+        help='level of runtime output, larger == more',
+        type=int,
+        default=1)
+    parser.add_argument(
+        'node_id',
+        help='Numeric node id',
+        type=int)
+    parser.add_argument(
+        'hostname',
+        help='ToRMS host running the Librarian',
+        type=str)
+    parser.add_argument(
+        'onetime',
+        help='ONe-time command (then exit)',
+        nargs='*',
+        default=None)
     args = parser.parse_args(sys.argv[1:])
 
     # argparse "choices" expands this in help message, less than helpful
     assert 1 <= args.node_id < 1000, 'Node ID must be from 1 - 999'
+    verbose = args.verbose
     main(args.node_id, args.hostname, args.onetime)
