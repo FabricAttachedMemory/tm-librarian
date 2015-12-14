8e5da18cea779a54f0d8bfdb77c02d8da392530d
Rocky Craig <rocky.craig@hpe.com>
2015-12-13 21:55:36 -0700

    Change book_policy.py collection of routines and constants into a class; implement two policies

--- book_policy.py
+++ book_policy.py
@@ -17,69 +17,82 @@ from frdnode import FRDnode, FRDintlv_group
 # routines.  xattr_assist does more and does the math for some of the
 # user.LFS.xxxxx intrinsics.  Start with general logic checks.
 
-BOOK_POLICY_DEFAULT = 'LocalOnly'
-_policies = (BOOK_POLICY_DEFAULT, 'LocalFirst', 'Random')
 
-def xattr_assist(LCEobj, cmdict, setting=False, removing=False):
-    LCEobj.errno = errno.EINVAL
-    xattr = cmdict['xattr']
-    value = cmdict.get('value', None)
-    if setting:
-        assert value is not None, 'Trying to set a null value'
-    elems = xattr.split('.')
-    if elems[1] != 'LFS':       # simple get or set, leave it to caller
+class BookPolicy(object):
+
+    POLICY_DEFAULT = 'LocalOnly'
+    _policies = (POLICY_DEFAULT, 'LocalFirst', 'Random')
+
+    @classmethod
+    def xattr_assist(cls, LCEobj, cmdict, setting=False, removing=False):
+        '''More error checking and legwork for user.LFS.xxxxx.  LCEobj is
+           both data to be used and returns the error on a Raise.'''
+        LCEobj.errno = errno.EINVAL
+        xattr = cmdict['xattr']
+        value = cmdict.get('value', None)
+        if setting:
+            assert value is not None, 'Trying to set a null value'
+        elems = xattr.split('.')
+        if elems[1] != 'LFS':       # simple get or set, leave it to caller
+            return (xattr, value)
+
+        # LFS special values
+        assert len(elems) == 3, 'LFS xattrs are of form "user.LFS.xxx"'
+        assert not removing, 'Removal of LFS xattrs is prohibited'
+
+        if elems[2] == 'AllocationPolicy':
+            if setting:
+                assert value in cls._policies, \
+                    'Bad AllocationPolicy "%s"' % value
+        elif elems[2] == 'Interleave':
+            assert not setting, 'Setting Interleave is prohibited'
+            shelf = LCEobj.cmd_get_shelf(cmdict)
+            bos = LCEobj.db.get_books_on_shelf(shelf)
+            value = bytes([ b.intlv_group for b in bos ]).decode()
+        else:
+            raise AssertionError('Bad LFS attribute "%s"' % xattr)
         return (xattr, value)
 
-    # LFS special values
-    assert len(elems) == 3, 'LFS xattrs are of form "user.LFS.xxx"'
-    assert not removing, 'Removal of LFS xattrs is prohibited'
+   #-----------------------------------------------------------------------
+    # Return a list of books or raise an error.
 
-    if elems[2] == 'AllocationPolicy':
-        if setting:
-            assert value in _policies, 'Bad AllocationPolicy "%s"' % value
-    elif elems[2] == 'Interleave':
-        assert not setting, 'Setting Interleave is prohibited'
-        shelf = LCEobj.cmd_get_shelf(cmdict)
-        bos = LCEobj.db.get_books_on_shelf(shelf)
-        value = bytes([ b.intlv_group for b in bos ]).decode()
-    else:
-        raise AssertionError('Bad LFS attribute')
-    return (xattr, value)
-
-###########################################################################
-# Return a list of books or raise an error.  LCEobj has an IGs member, BTW.
-
-
-def _policy_LocalOnly(LCEobj, shelf, cmdict, books_needed, inverse=False):
-    # using IGs 0-79 on nodes 1-80
-    IG = node_id = cmdict['context']['node_id'] - 1
-    freebooks = LCEobj.db.get_books_by_intlv_group(
-        IG, TMBook.ALLOC_FREE, books_needed, inverse)
-    return freebooks
-
-
-def _policy_LocalFirst(LCEobj, shelf, cmdict, books_needed):
-    localbooks = _policy_LocalOnly(LCEobj, shelf, cmdict, books_needed)
-    books_needed -= len(localbooks)
-    assert books_needed >= 0, 'LocalFirst policy internal error'
-    if not books_needed:
-        return localbooks
-    nonlocalbooks = _policy_LocalOnly(LCEobj, shelf, cmdict, books_needed,
-                                      inverse=True)
-    return localbooks.extend(nonlocalbooks)
-
-
-def get_books_by_policy(LCEobj, shelf, cmdict, books_needed):
-    LCEobj.errno = errno.EINVAL
-    policy = LCEobj.db.get_xattr(shelf, 'user.LFS.AllocationPolicy')
-    assert policy in _policies, 'Unknown policy "%s"' % policy
-
-    if policy == BOOK_POLICY_DEFAULT:
-        freebooks = _policy_LocalOnly(LCEobj, shelf, cmdict, books_needed)
-    elif policy == 'LocalFirst':
-        freebooks = _policy_LocalFirst(LCEobj, shelf, cmdict, books_needed)
-    else:
+    def __init__(self, LCEobj, shelf, context):
+        assert len(LCEobj.IGs) == len(LCEobj.nodes), 'IG:node != 1:1'
         LCEobj.errno = errno.EINVAL
-        raise AssertionError('%s not implemented yet' % policy)
+        self.LCEobj = LCEobj
+        self.shelf = shelf
+        self.context = context
+        self.name = LCEobj.db.get_xattr(shelf, 'user.LFS.AllocationPolicy')
+        assert self.name in self._policies, 'Unknown policy "%s"' % self.name
 
-    return freebooks
+    def __str__(self):
+        return '%s policy=%s' % (self.shelf.name, self.name)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def _policy_LocalOnly(self, books_needed, inverse=False):
+        # using IGs 0-79 on nodes 1-80
+        IG = self.context['node_id'] - 1
+        db = self.LCEobj.db
+        freebooks = db.get_books_by_intlv_group(
+            IG, TMBook.ALLOC_FREE, books_needed, inverse)
+        return freebooks
+
+    def _policy_LocalFirst(self, books_needed):
+        localbooks = self._policy_LocalOnly(books_needed)
+        books_needed -= len(localbooks)
+        assert books_needed >= 0, 'LocalFirst policy internal error'
+        if not books_needed:
+            return localbooks
+        nonlocalbooks = self._policy_LocalOnly(books_needed, inverse=True)
+        return localbooks + nonlocalbooks
+
+    def __call__(self, books_needed):
+        self.LCEobj.errno = errno.EINVAL
+        if self.name == self.POLICY_DEFAULT:
+            return self._policy_LocalOnly(books_needed)
+        elif self.name == 'LocalFirst':
+            return self._policy_LocalFirst(books_needed)
+        else:
+            raise AssertionError('%s not implemented yet' % self.name)
