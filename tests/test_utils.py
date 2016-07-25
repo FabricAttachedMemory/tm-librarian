546e2cd3b173c17ac69af9ef074eca862b4fb876
John Wiens <john@debian-john-workstation>
2016-07-25 09:33:38 -0600

    Added Limit testing for shell functionallity

---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+# These are common functions that may be used from other scripts
+
+
+#remove things from system cleanly
+def silent_remove(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+#Alternate form of silent remove that truncates to 0 first (Use with TMAS)
+def zero_silent_remove(filename):
+    #subprocess.call(['truncate','-s0G',filename])
+    silent_remove(filename)
+
+#function that returns the absolute path to the tm-librarian folder
+def get_librarian_path():
+    real_path = os.path.dirname(os.path.realpath(__file__))
+    librarian_path = real_path[:real_path.rfind('/')]
+    return librarian_path
