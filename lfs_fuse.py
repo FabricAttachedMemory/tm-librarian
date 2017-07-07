67d3e6dbbd45967b872d40c93fd7cb0cb22ef461
Ross Lannen <ross.lannen@gmail.com>
2017-07-07 09:28:12 -0600

    removed some set traces

--- lfs_fuse.py
+++ lfs_fuse.py
@@ -628,7 +628,6 @@ class LibrarianFS(Operations):  # Name shows up in mount point
         # looking for filehandles?  See FUSE docs.  Librarian will do
         # all access calculations so call it first.
         rsp = self.librarian(self.lcp('open_shelf', path=path))
-        set_trace()
         shelf = TMShelf(rsp)
         self.get_bos(shelf)
         # File handle is a proxy for FuSE to refer to "real" file descriptor.
@@ -699,7 +698,6 @@ class LibrarianFS(Operations):  # Name shows up in mount point
         # Refresh shelf info
         rsp = self.librarian(self.lcp('get_shelf', path=path))
         shelf = TMShelf(rsp)
-        set_trace()
         if shelf.size_bytes < length:
             raise TmfsOSError(errno.EINVAL)
         self.get_bos(shelf)
