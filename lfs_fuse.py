29be1ab2bcbf7f9ef2556daa3d5f19bd698b3e66
Ross Lannen <ross.lannen@gmail.com>
2017-07-10 11:38:41 -0600

    for real this time

--- lfs_fuse.py
+++ lfs_fuse.py
@@ -606,7 +606,7 @@ class LibrarianFS(Operations):  # Name shows up in mount point
         zeroname = '%s%d' % (self._ZERO_PREFIX, shelf.id)
         if zeroname != shelf.name:
 
-            self.rename(get_shelf_path(shelf), zeroname)
+            self.rename(self.get_shelf_path(shelf), zeroname)
             shelf.name = zeroname
             shelf.mode = stat.S_IFREG   # un-block it as was done on server
         if self.verbose <= 3:
